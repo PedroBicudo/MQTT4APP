34a4b2dd523ff649621d4e6f6f2c8964763610ac
SRKFY <pedrobicudoa@gmail.com>
2018-10-27 16:40:20 -0300

    10/27/18

---
+++ MQTT4APP/mosquitto_config.py
@@ -0,0 +1,24 @@
+# MQTT CONFIGURATION
+mqtt_server = "127.0.0.1"
+mqtt_port = "1883"
+mqtt_ID = "MYGARDEN"
+mqtt_user = "mygarden"
+mqtt_passwd = "123"
+"""
+Available types of Qos
+0, 1, 2
+"""
+mqtt_qos = 1
+
+# BACK 4 APP CONFIGURATION
+"""
+topics:
+id  -  topico -  valor
+
+"""
+back_app_id = "APP ID"
+back_rest_id = "REST ID"
+
+
+
+
