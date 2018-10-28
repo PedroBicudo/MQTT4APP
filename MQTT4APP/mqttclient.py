11240792df2dd6f220f4cb984503be1ac14968e6
SRKFY <43938917+SRKFY@users.noreply.github.com>
2018-10-27 21:31:43 -0300

    Update mqttclient.py

--- MQTT4APP/mqttclient.py
+++ MQTT4APP/mqttclient.py
@@ -1,6 +1,5 @@
 import paho.mqtt.client as mqtt
 import connectors as cn
-#from os import system
 from mosquitto_config import \
     mqtt_ID, \
     mqtt_server, \
@@ -16,60 +15,12 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    topico_mqtt = str(msg.topic)
+    _id = str(msg.topic[12:17])
+    topico = str(msg.topic[18:])
     valor = int(msg.payload)
 
-    # Variaveis filtro
-    topic_b, topic_e = 0, 12
-    id_b, id_e = 0, 3
-
-    # Topic ID
-    while True:
-        topic_b += 1
-        topic_e += 1
-        if topico_mqtt[topic_b:topic_e - 1:] == "temperatura":
-            topic_e -= 1
-            break
-        elif topico_mqtt[topic_b:topic_e:] == "luminosidade":
-            break
-        else:
-            if topico_mqtt[topic_b:topic_e - 5:] == "umidade":
-                topic_e -= 5
-                break
-            if topic_b > len(topico_mqtt) or topic_b > len(topico_mqtt):
-                #print("NÃo encontrado")
-                break
-
-    # Sensor ID
-    while True:
-        id_b += 1
-        id_e += 1
-        if topico_mqtt[id_b:id_e:] == "id_":
-            id_b += 3
-            id_e += 5
-            break
-        if id_b > len(topico_mqtt) or id_b > len(topico_mqtt):
-            #print("NÃo encontrado")
-            break
-
-    # Define sensor e identificação
-    sensor = topico_mqtt[topic_b:topic_e:]
-    _id = topico_mqtt[id_b:id_e:]
-
-    # Verificar se variavel existe
-    if sensor and _id:
-        print("""
-        Topico: %s
-        ID: %s 
-        Valor: %d\n\n
-        Resultado:""" %( sensor, _id, valor)
-              )
-    else:
-        print("Erro, algum valor não foi definido corretamente")
-        exit(1)
-
-    # Conectar
-    cn.connect_back4app(_id, sensor, valor)
+    print(topico)
+    cn.connect_back4app(_id, topico, valor)
 
 
 client = mqtt.Client( "%s" % mqtt_ID )
@@ -81,8 +32,8 @@ if mqtt_user and mqtt_passwd:
     client.connect("%s" % mqtt_server, int(mqtt_port), 60)
     client.loop_forever()
 else:
-    print("Usuário ou senha não informados.")
-    print("Falha na conexao.")
+    print("Usuário ou senha não informados")
+    print("Falha na conexao")
 
 
 
