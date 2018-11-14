455eb0336886d2b2d1cfce75ab794bf714565e6a
SRKFY <43938917+SRKFY@users.noreply.github.com>
2018-11-14 10:50:59 -0200

    2018/11/14

---
+++ Sensor_Simulator/mosquitto_config.py
@@ -0,0 +1,9 @@
+# MQTT CONFIGURATION
+mqtt_server = "127.0.0.1"
+mqtt_port = "1883"
+mqtt_ID = "SENSOR-PUB"
+mqtt_user = "mygarden"
+mqtt_passwd = "123"
+
+
+
---
+++ Sensor_Simulator/mqttclient.py
@@ -0,0 +1,49 @@
+import paho.mqtt.publish as mqtt
+import random as rd
+import time as tm
+from mosquitto_config import \
+    mqtt_ID, \
+    mqtt_server, \
+    mqtt_port, \
+    mqtt_user, \
+    mqtt_passwd
+
+print("""
+==========================================================================
+=	 ____  _____ _   _ ____   ___  ____                              =
+=	/ ___|| ____| \ | / ___| / _ \|  _ \                             =
+=	\___ \|  _| |  \| \___ \| | | | |_) |                            =
+=	 ___) | |___| |\  |___) | |_| |  _ <                             =
+=	|____/|_____|_| \_|____/ \___/|_| \_\                            =
+=                                                                        =
+=           ____ ___ __  __ _   _ _        _  _____ ___  ____            =
+=           / ___|_ _|  \/  | | | | |      / \|_   _/ _ \|  _ \          =
+=           \___ \| || |\/| | | | | |     / _ \ | || | | | |_) |         =
+=            ___) | || |  | | |_| | |___ / ___ \| || |_| |  _ <          =
+=           |____/___|_|  |_|\___/|_____/_/   \_\_| \___/|_| \_\         =
+=                                                                        =
+==========================================================================
+
+--------------------------------------------------------------------------
+INICIANDO...
+--------------------------------------------------------------------------
+""")
+tm.sleep(5.0)
+while True:
+    valores=rd.sample(range(0,100), 4)
+    msgs=[{
+        'topic':'/home/horta/%s/temperatura' % mqtt_ID,
+        'payload': '%d' % valores[1]
+    },{
+        'topic':'/home/horta/%s/umidade' % mqtt_ID,
+        'payload': '%d' % valores[2]
+   },
+    {
+        'topic':'/home/horta/%s/luminosidade' % mqtt_ID,
+        'payload': '%d' % valores[3]
+    }]
+
+    print(msgs)
+    mqtt.multiple(msgs, hostname='127.0.0.1', port=1883, client_id=mqtt_ID, auth={'username': 'mygarden', 'password': '123'} )
+    tm.sleep(10.0)
+
