af0556ff25947745849f966731628c596702cfaf
SRKFY <43938917+SRKFY@users.noreply.github.com>
2018-12-03 16:52:13 -0200

    03/12/2018
    Ampliando quantidade de sensores e nomes

--- MQTT4APP/mosquitto_config.py
+++ MQTT4APP/mosquitto_config.py
@@ -6,6 +6,20 @@ mqtt_user = "client"
 mqtt_passwd = "123"
 mqtt_qos = 1
 
+# Nome(s) NodeMCU
+mic_id = [
+	"node",
+	"teste",
+	"brazil"
+		]
+
+# Tipos de sensores
+mic_sensors = [
+	"temperatura",
+	"umidade",
+	"luminosidade"
+			]
+
 # BACK 4 APP CONFIGURATION
 back_app_id = " APP ID"
 back_rest_id = " REST ID"
