92d14fa150a7757ca74d3b9cc78713c77528f786
Pedro Henrique <pedroabicudo_2012@hotmail.com>
2019-12-14 17:54:27 -0300

    Adicionado diretorio com testes unitarios.

---
+++ tests/test_mqtt4app.py
@@ -0,0 +1,36 @@
+"""Testes unitarios em mqtt4app."""
+from mqtt4app import Mqtt4App
+import unittest
+
+
+class Mqtt4AppTests(unittest.TestCase):
+    """Classes de testes unitarios."""
+
+    def setUp(self):
+        self.mqtt4app = Mqtt4App(
+            topics=['a', 'b', 'c'], 
+            back_db_name='teste',
+            transport="websockets"
+            )
+        self.mqtt4app.connect("broker.mqttdashboard.com", 8000)
+
+    def test_connection(self):
+        pass
+
+    def test_tuple_to_list(self):
+        result = self.mqtt4app._convert_to_tuple_list(list('abc'))
+        answer =[('a', 0), ('b', 0), ('c', 0)]
+        self.assertEqual(
+            result, answer,
+            f"{result} deveria ser {answer}."
+            )
+
+    def test_get_class_path(self):
+        result = self.mqtt4app._get_class_path()
+        self.assertEqual(
+            result, "/classes/teste/", 
+            f"{result} deveria ser /classes/teste/"
+        )
+
+    def tearDown(self):
+        self.mqtt4app.disconnect()
